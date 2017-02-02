8a4b0478376b446895714e77ee542d1feee66bec
Andrew Haines <andrew@haines.org.nz>
2017-02-02 10:22:44 +0000

    Initial commit

---
+++ src/test/java/redbadger/Example1Test.java
@@ -0,0 +1,29 @@
+package redbadger;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Example1Test {
+    @Test
+    public void shouldPass() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void shouldFail() {
+        assertEquals(1, 2);
+    }
+
+    @Test
+    @Ignore
+    public void shouldBeSkipped() {
+
+    }
+
+    @Test
+    public void shouldError() {
+        throw new IllegalStateException();
+    }
+}
